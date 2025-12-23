a9f69f999f16550b322b11b6f193babfaf8e1a20
Tiago Santos <tvcsantos@gmail.com>
2025-12-23 16:21:24 +0000

    fix: remove dist

--- dist/schema/configuration.js
+++
@@ -1,7 +0,0 @@
-import { mqttProviderSchema } from './mqtt-configuration.js';
-import { restProviderSchema } from './rest-configuration.js';
-import z from 'zod';
-export const configurationSchema = z.discriminatedUnion('provider', [
-    restProviderSchema,
-    mqttProviderSchema,
-]);
--- dist/schema/status-query.js
+++
@@ -1,7 +0,0 @@
-export const statusQuerySchema = {
-    type: 'object',
-    properties: {
-        id: { type: 'number' },
-    },
-    required: ['id'],
-};
