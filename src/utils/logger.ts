7a873c67125743892a3802947e060f86c335e109
Tiago Santos <tvcsantos@gmail.com>
2026-09-13 14:14:45 +0100

    feat: use pino-pretty only on development

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,16 +1,19 @@
 import pino from 'pino';
 
 export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname',
-      messageFormat: '{msg}',
-      singleLine: true,
-      hideObject: false,
-    },
-  },
+  transport:
+    process.env.NODE_ENV === 'development'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'HH:MM:ss',
+            ignore: 'pid,hostname',
+            messageFormat: '{msg}',
+            singleLine: true,
+            hideObject: false,
+          },
+        }
+      : undefined,
   level: process.env.V2CA_LOG_LEVEL || 'info',
 });
